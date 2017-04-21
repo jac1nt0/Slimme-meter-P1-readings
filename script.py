63c299a33f004813f0d74e67da2f1b0193d02e38
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-22 00:18:37 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,6 @@ import serial
 import time
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
-for x in range(10):
-  while msg=ser.readline() ! NULL:
-    print msg
-  time.sleep(1)
+
+while True:
+  print ser.readline()
