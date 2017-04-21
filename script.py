b3a85048d1932004473aee2287db8e6f627e7714
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-22 00:39:41 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity = serial.PARITY_ODD, stopbits=serial.STOPBITS_NONE, bytesize = serial.EIGHTBITS)
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
 
 for x in range(5):
   msg = []
