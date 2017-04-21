5f3d9091647a3cb5f5ae0cda4c6c5d2f69c792d9
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-22 00:38:53 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import time
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity = serial.PARITY_ODD, stopbits=serial.STOPBITS_NONE, bytesize = serial.EIGHTBITS)
 
-while True:
+for x in range(5):
   msg = []
   line = ser.readline()
   msg.append(line)
