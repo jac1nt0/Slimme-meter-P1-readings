c05585e771b222e610cf72ec06213aeba7bf6a41
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-22 00:43:53 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,9 +6,7 @@ ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
 for x in range(5):
   msg = []
   while True:
-    line = ser.readline()
-    print line
-    msg.append(line)
-    if '!' in line:
+    msg.append(ser.readline())
+    if '!' in msg[-1]:
       print msg
       break
