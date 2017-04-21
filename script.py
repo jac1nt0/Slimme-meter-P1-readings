afa6cb01eb90073cdf49d5320be250915772cf80
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-22 00:38:26 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import serial
 import time
 
-ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
+ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity = serial.PARITY_ODD, stopbits=serial.STOPBITS_NONE, bytesize = serial.EIGHTBITS)
 
 while True:
-  print ser.readline()
+  msg = []
+  line = ser.readline()
+  msg.append(line)
+  if '!' in line:
+    print msg
