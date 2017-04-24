562fa019cf7e4e1349eb8f6bf32853f3d5fa19f3
Jacinto Garcia <jacintogm84@gmail.com>
2017-04-24 23:44:02 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -32,11 +32,10 @@ sentence4 = OBIS + pp.Suppress('(') +pp.Optional(pp.Word(pp.nums)) + pp.Suppress
 
 CRC = pp.Suppress(pp.Literal("!")) + pp.Word(pp.alphanums,exact=4)('crc')
 
-grammar = header + pp.OneOrMore(pp.OneOrMore(sentence1) ^ pp.OneOrMore(sentence2) ^ pp.OneOrMore(sentence3) ^ pp.OneOrMore(sentence4)) + CRC
+grammar = header + pp.OneOrMore(sentence1 ^ sentence2 ^ sentence3 ^ sentence4) + CRC
 
 test_data = '''
 /KFM5KAIFA-METER
-
 1-3:0.2.8(42) 
 0-0:1.0.0(170424212445S)
 0-0:96.1.1(4530303235303030303834323232373136)
