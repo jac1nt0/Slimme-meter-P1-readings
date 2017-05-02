07aa9077886ada9e517737344e1d62309e3cec5b
Jacinto Garcia <jacintogm84@gmail.com>
2017-05-02 16:15:40 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import pyparsing
 import logging
 
 log = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+log.setLevel(logging.DEBUG)
 fh = logging.FileHandler(filename='example.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
