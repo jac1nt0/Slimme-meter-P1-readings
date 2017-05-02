5f21c53f40d69bce7a81e2b175a243e20728e06d
Jacinto Garcia <jacintogm84@gmail.com>
2017-05-02 15:36:34 +0200

    Update script.py

--- script.py
+++ script.py
@@ -20,41 +20,41 @@ def meter():
   }
     
     
-    def __init__(self, port='/dev/ttyUSB0', **kwargs):
-      config = {}
-      config.update(self.defaults)
-      config.update(kwargs)
+  def __init__(self, port='/dev/ttyUSB0', **kwargs):
+    config = {}
+    config.update(self.defaults)
+    config.update(kwargs)
 
-      log.debug('Open serial connect to {} with: {}'.format(port, ', '.join('{}={}'.format(key, value) for key, value in config.items())))
+    log.debug('Open serial connect to {} with: {}'.format(port, ', '.join('{}={}'.format(key, value) for key, value in config.items())))
 
-      try:
-        self.serial = serial.Serial(port, **config)
-      except (serial.SerialException,OSError) as e:
-        raise SmartMeterError(e)
-      else:
-        self.serial.setRTS(False)
-        self.port = self.serial.name
+    try:
+      self.serial = serial.Serial(port, **config)
+    except (serial.SerialException,OSError) as e:
+      raise SmartMeterError(e)
+    else:
+      self.serial.setRTS(False)
+      self.port = self.serial.name
 
-      log.info('New serial connection opened to %s', self.port)
-        
-    def connect(self):
-      if not self.serial.isOpen():
-        log.info('Opening connection to `%s`', self.serial.name)
-        self.serial.open()
-        self.serial.setRTS(False)
-      else:
-        log.debug('`%s` was already open.', self.serial.name)
+    log.info('New serial connection opened to %s', self.port)
 
-
-    def disconnect(self):
-      if self.serial.isOpen():
-        log.info('Closing connection to `%s`.', self.serial.name)
-        self.serial.close()
-      else:
-        log.debug('`%s` was already closed.', self.serial.name)
+  def connect(self):
+    if not self.serial.isOpen():
+      log.info('Opening connection to `%s`', self.serial.name)
+      self.serial.open()
+      self.serial.setRTS(False)
+    else:
+      log.debug('`%s` was already open.', self.serial.name)
 
 
-    def connected(self):
-      retur n self.serial.isOpen()
+  def disconnect(self):
+    if self.serial.isOpen():
+      log.info('Closing connection to `%s`.', self.serial.name)
+      self.serial.close()
+    else:
+      log.debug('`%s` was already closed.', self.serial.name)
+
+
+  def connected(self):
+    retur n self.serial.isOpen()
       
 
