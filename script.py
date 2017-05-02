fc945a37b8845a6724ab38ed49e94b24eede5678
Jacinto Garcia <jacintogm84@gmail.com>
2017-05-02 16:35:14 +0200

    Update script.py

--- script.py
+++ script.py
@@ -97,6 +97,104 @@ class meter():
     log.debug('Constructing P1Packet from raw data')
 
     return P1Packet(datagram)
+  
+  def __enter__(self):
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.disconnect()
+    
+    
+    
+class SmartMeterError(Exception):
+  pass
+
+class P1PacketError(Exception):
+  pass
+
+class P1Packet(object):
+  _datagram = ''
+
+  def __init__(self, datagram):
+    self._datagram = datagram
+
+    self.validate()
+
+    keys = {}
+    keys['header'] = self.get(b'^\s*(/.*)\r\n')
+
+    keys['kwh'] = {}
+    keys['kwh']['eid'] = self.get(b'^0-0:96\.1\.1\(([^)]+)\)\r\n')
+    keys['kwh']['tariff'] = self.get_int(b'^0-0:96\.14\.0\(([0-9]+)\)\r\n')
+    keys['kwh']['switch'] = self.get_int(b'^0-0:96\.3\.10\((\d)\)\r\n')
+    keys['kwh']['treshold'] = self.get_float(b'^0-0:17\.0\.0\(([0-9]{4}\.[0-9]{2})\*kW\)\r\n')
+
+    keys['kwh']['low'] = {}
+    keys['kwh']['low']['consumed'] = self.get_float(b'^1-0:1\.8\.1\(([0-9]+\.[0-9]+)\*kWh\)\r\n')
+    keys['kwh']['low']['produced'] = self.get_float(b'^1-0:2\.8\.1\(([0-9]+\.[0-9]+)\*kWh\)\r\n')
+
+    keys['kwh']['high'] = {}
+    keys['kwh']['high']['consumed'] = self.get_float(b'^1-0:1\.8\.2\(([0-9]+\.[0-9]+)\*kWh\)\r\n')
+    keys['kwh']['high']['produced'] = self.get_float(b'^1-0:2\.8\.2\(([0-9]+\.[0-9]+)\*kWh\)\r\n')
+
+    keys['kwh']['current_consumed'] = self.get_float(b'^1-0:1\.7\.0\(([0-9]+\.[0-9]+)\*kW\)\r\n')
+    keys['kwh']['current_produced'] = self.get_float(b'^1-0:2\.7\.0\(([0-9]+\.[0-9]+)\*kW\)\r\n')
+
+    keys['gas'] = {}
+    keys['gas']['eid'] = self.get(b'^0-1:96\.1\.0\(([^)]+)\)\r\n')
+    keys['gas']['device_type'] = self.get_int(b'^0-1:24\.1\.0\((\d)+\)\r\n')
+    keys['gas']['total'] = self.get_float(b'^(?:0-1:24\.2\.1(?:\(\d+[SW]\))?)?\(([0-9]{5}\.[0-9]{3})(?:\*m3)?\)\r\n', 0)
+    keys['gas']['valve'] = self.get_int(b'^0-1:24\.4\.0\((\d)\)\r\n')
+
+    keys['msg'] = {}
+    keys['msg']['code'] = self.get(b'^0-0:96\.13\.1\((\d+)\)\r\n')
+    keys['msg']['text'] = self.get(b'^0-0:96\.13\.0\((.+)\)\r\n')
+
+    self._keys = keys
+
+
+  def __getitem__(self, key):
+    return self._keys[key]
+
+
+  def get_float(self, regex, default=None):
+    result = self.get(regex, None)
+    if not result:
+      return default
+    return float(self.get(regex, default))
+
+
+  def get_int(self, regex, default=None):
+    result = self.get(regex, None)
+    if not result:
+      return default
+    return int(result)
+
+
+  def get(self, regex, default=None):
+    results = re.search(regex, self._datagram, re.MULTILINE)
+    if not results:
+      return default
+    return results.group(1).decode('ascii')
+
+
+  def validate(self):
+    pattern = re.compile(b'\r\n(?=!)')
+    for match in pattern.finditer(self._datagram):
+      packet = self._datagram[:match.end() + 1]
+      checksum = self._datagram[match.end() + 1:]
+
+    if checksum.strip():
+      given_checksum = int('0x' + checksum.decode('ascii').strip(), 16)
+      calculated_checksum = crc16(packet)
+
+      if given_checksum != calculated_checksum:
+        log.error('Checksum mismatch: given={}, calculated={}'.format(given_checksum, calculated_checksum))
+        raise P1PacketError('P1Packet with invalid checksum found')
+
+
+  def __str__(self):
+    return self._datagram.decode('ascii')
       
 
 if __name__=='__main__':
