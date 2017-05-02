f4dd4068d2d0565d7d2e9f07fffb00474a945f68
Jacinto Garcia <jacintogm84@gmail.com>
2017-05-02 16:58:50 +0200

    Update script.py

--- script.py
+++ script.py
@@ -16,11 +16,6 @@ log.addHandler(fh)
 class meter():
   defaults = {
     'baudrate' : 115200,
-    'bytesize': serial.EIGHTBITS,
-    'parity': serial.PARITY_NONE,
-    'stopbits': serial.STOPBITS_ONE,
-    'xonxoff': False,
-    'timeout': 10,
   }
   
   def __init__(self, port='/dev/ttyUSB0', **kwargs):
